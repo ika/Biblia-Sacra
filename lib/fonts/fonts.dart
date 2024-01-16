ea056304103d44f00a4a2bf2cfe51e043ae9eb81
Ian Armstrong <ian.k.armstrong@gmail.com>
2024-01-16 17:25:53 +0200

    update

--- lib/fonts/fonts.dart
+++ lib/fonts/fonts.dart
@@ -1,6 +1,5 @@
 import 'package:bibliasacra/bloc/bloc_font.dart';
 import 'package:bibliasacra/globals/globals.dart';
-import 'package:bibliasacra/main/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
